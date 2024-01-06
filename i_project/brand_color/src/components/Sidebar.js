d753cf102fac1dc5945d549540672a9bcf81d578
abdullah <105575210+abdullahjsd@users.noreply.github.com>
2024-01-06 22:05:42 +0300

    sidebar

---
+++ i_project/brand_color/src/components/Sidebar.js
@@ -0,0 +1,20 @@
+ 
+
+function Sidebar(){
+    return(
+        <aside className="sidebar">
+            <a>Brand <b>Colors</b></a>
+            <div className="description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati reiciendis nihil distinctio tempore aliquid aperiam.
+            </div>
+            <nav className="menu">
+                <ul>
+                    <li>
+                        <a>About BrondColors</a>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    )
+}
+export default Sidebar;
